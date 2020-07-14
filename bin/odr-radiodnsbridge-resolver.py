f557ad9744977b0c69bb2f386a10e57ba60593e6
Nick Piggott <nick@piggott.eu>
2020-07-14 17:05:54 +0100

    Make package installable
    Add setup.py (thanks to Ulrik Brinck) and define python shell explicitly for the executable

--- bin/odr-radiodnsbridge-resolver.py
+++ bin/odr-radiodnsbridge-resolver.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 #===============================================================================
 # OpenDigitalRadio - RadioDNS Bridge - Resolver Code
 # 
--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
 setup(
     name='odr-radiodns-bridge',
     version='1.0',
     packages=['odr', 'odr.radiodns'],
+    package_dir = {'' : 'src'}
     url='https://github.com/nickpiggott/odr-radiodns-bridge',
     license='GNU Lesser General Public License 2.1',
     author='Nick Piggott',
