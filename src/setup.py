4be9c601a6daf2c2dffe9e36099ec3c912a9b5d0
Nick Piggott <nick@piggott.eu>
2020-07-14 17:00:53 +0100

    Create setup.py
    Added a setup.py (thanks to Ulrik Brinck)

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='odr-radiodns-bridge',
+    version='1.0',
+    packages=['odr', 'odr.radiodns'],
+    url='https://github.com/nickpiggott/odr-radiodns-bridge',
+    license='GNU Lesser General Public License 2.1',
+    author='Nick Piggott',
+    author_email='nick@piggott.eu',
+    description='Tools to bridge RadioDNS applications into the OpenDigitalRadio environment'
+)
